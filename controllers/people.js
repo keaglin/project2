45f519c3c4eb2c49d46136852b948c7421725706
Kevon <kceaglin@gmail.com>
2018-01-09 15:39:09 -0500

    controller scaffolding

--- controllers/people.js
+++ controllers/people.js
@@ -1,6 +1,34 @@
-const express = require('express')
-const router  = express.Router()
+const express  = require('express')
+const router   = express.Router()
+const mongoose = require('../db/connection')
+const Person   = mongoose.model('Person')
 
+router.get('/', (req, res) => {
+  res.render('index')
+})
+router.post('/', (req, res) => {
+
+})
+router.put('/', (req, res) => {
+
+})
+router.delete('/', (req, res) => {
+
+})
+
+// function getPeople() {
+//   (req, res) => {
+//     res.send('hello world')
+//   }
+// }
+// function addPerson() {}
+// function editPerson() {}
+// function removePerson() {}
+
+// router.get('/', getPeople)
+// router.post('/', addPerson)
+// router.put('/', editPerson)
+// router.delete('/', removePerson)
 
 
 module.exports = router
