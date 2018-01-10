d15cb24f0c212a626c554ad8a2f06e5cb785522d
Kevon <kceaglin@gmail.com>
2018-01-09 19:33:05 -0500

    mvp

--- controllers/people.js
+++ controllers/people.js
@@ -3,18 +3,9 @@ const router   = express.Router()
 const mongoose = require('../db/connection')
 const Person   = mongoose.model('Person')
 
-router.get('/', (req, res) => {
-  res.render('index')
-})
-router.post('/', (req, res) => {
-
-})
-router.put('/', (req, res) => {
-
-})
-router.delete('/', (req, res) => {
-
-})
+// copied routes from book-e-backend 
+// 1 to reduce typos
+// 2 because I don't have put, post and delete solidified yet
 
 // function getPeople() {
 //   (req, res) => {
@@ -29,6 +20,47 @@ router.delete('/', (req, res) => {
 // router.post('/', addPerson)
 // router.put('/', editPerson)
 // router.delete('/', removePerson)
+
+router.get('/', (req, res) => {
+  Person.find({})
+    .then((people) => {
+      res.render('index', {
+        people: people
+      })
+    })
+})
+
+router.get('/:name', (req, res) => {
+  var name = req.params.name
+  Person.findOne({ name: name })
+  // filter based on the name attribute, as defined in models
+  // use the name variable that we defined to get the one record
+  // we want
+  .then(person => {
+    res.render('person', { person: person })
+  })
+})
+
+router.post('/', (req, res) => {
+  Person.create(req.body.person)
+    .then(person => {
+      res.redirect(`/people/${person.name}`)
+    })
+})
+
+router.put('/:name', (req, res) => {
+  Person.findOneAndUpdate({ name: req.params.name }, req.body.person, { new: true })
+  .then(person => {
+    res.redirect(`/people/${person.name}`)
+  })
+})
+
+router.delete('/:name', (req, res) => {
+  Person.findOneAndRemove({ name: req.params.name })
+  .then(person => {
+    res.redirect('/people')
+  })
+})
 
 
 module.exports = router
--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const express          = require('express')
-const pug              = require('pug')
+// const pug              = require('pug')
+const hbs              = require('express-handlebars')
 const PeopleController = require('./controllers/people')
+const parser           = require('body-parser')
 const app              = express()
 
 app.set('port', process.env.PORT || 3001)
 
-app.set('view engine', 'pug')
+// app.set('view engine', 'pug')
+app.set('view engine', 'hbs')
+app.engine('.hbs', hbs({
+  extname:        '.hbs',
+  partialsDir:    'views/',
+  layoutsDir:     'views/',
+  defaultLayout:  'layout'
+}))
 
 app.use('/assets', express.static('public'))
 
 // app.get('/', (req, res) => {
 //   res.render('index')
 // })
-
-app.use('/', PeopleController)
+app.use(parser.urlencoded({ extended: true }))
+app.get('/', (req, res) => {
+  res.redirect('/people')
+})
+app.use('/people', PeopleController)
 
 app.listen(app.get('port'), () => {
   console.log('It\'s aliiive! on PORT', app.get('port'))
