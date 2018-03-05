1074ccba4620194a848a02a1cc0d7f60716f8975
K. Eaglin <kceaglin@gmail.com>
2018-03-05 13:11:38 -0500

    updating routes for c-ud functionality

--- controllers/people.js
+++ controllers/people.js
@@ -3,28 +3,10 @@ const router   = express.Router()
 const mongoose = require('../db/connection')
 const Person   = mongoose.model('Person')
 
-// copied routes from book-e-backend 
-// 1 to reduce typos
-// 2 because I don't have put, post and delete solidified yet
-
-// function getPeople() {
-//   (req, res) => {
-//     res.send('hello world')
-//   }
-// }
-// function addPerson() {}
-// function editPerson() {}
-// function removePerson() {}
-
-// router.get('/', getPeople)
-// router.post('/', addPerson)
-// router.put('/', editPerson)
-// router.delete('/', removePerson)
-
 router.get('/', (req, res) => {
   Person.find({})
-    .then((people) => {
-      res.json({ people: people })
+    .then(people => {
+      res.json(people)
     })
 })
 
@@ -35,29 +17,33 @@ router.get('/:name', (req, res) => {
   // use the name variable that we defined to get the one record
   // we want
   .then(person => {
-    res.json({ person: person })
+    res.json(person)
   })
+  .catch(err => console.log('Woops!', err))
 })
 
 router.post('/', (req, res) => {
   Person.create(req.body.person)
     .then(person => {
-      res.redirect(`/people/${person.name}`)
+      res.json(book)
     })
+    .catch(err => console.log('Woops!', err))
 })
 
 router.put('/:name', (req, res) => {
   Person.findOneAndUpdate({ name: req.params.name }, req.body.person, { new: true })
   .then(person => {
-    res.redirect(`/people/${person.name}`)
+    res.json(person)
   })
+  .catch(err => console.log('Woops!', err))
 })
 
 router.delete('/:name', (req, res) => {
   Person.findOneAndRemove({ name: req.params.name })
   .then(person => {
-    res.redirect('/people')
+    res.json(person)
   })
+  .catch(err => console.log('Woops!', err))
 })
 
 
